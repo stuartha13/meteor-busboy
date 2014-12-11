24e4df7959c576eeaec00b25c7aeec0fb7370d72
Stuart Hammar <stuart.hammar@gmail.com>
2014-12-11 22:48:53 +0200

    Renamed the package files | Added a README

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'shammar13:busboy',
     summary: 'Add Busboy to Meteor',
-    version: '1.0.0',
+    version: '1.0.1',
     git: 'https://github.com/stuartha13/meteor-busboy.git'
 });
 
@@ -12,11 +12,11 @@ Npm.depends({
 Package.onUse(function(api) {
     api.versionsFrom('1.0');
     api.export('Busboy');
-    api.addFiles(['shammar13:busboy.js'], 'server');
+    api.addFiles(['busboy.js'], 'server');
 });
 
 Package.onTest(function(api) {
     api.use('tinytest');
-    api.use('shammar13:busboy');
-    api.addFiles('shammar13:busboy-tests.js');
+    api.use('busboy');
+    api.addFiles('busboy-tests.js');
 });
